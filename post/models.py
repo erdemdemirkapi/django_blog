06164dbc37d356a93497135fc54e654ec0915d87
erdem <demirkapierdem@gmail.com>
2019-05-09 17:29:56 +0300

    CRUD's are finished

--- post/models.py
+++ post/models.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 class Post(models.Model):
     title = models.CharField(max_length = 120)
     content = models.TextField()
-    publishing_date = models.DateTimeField()
+    publishing_date = models.DateTimeField(auto_now_add = True)
 
     def __str__(self):
         return self.title
--- post/views.py
+++ post/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, HttpResponse, get_object_or_404
+from django.shortcuts import render, HttpResponse, get_object_or_404, HttpResponseRedirect, redirect
 from .models import Post
+from .forms import PostForm
+from django.contrib import messages
 
 def post_index(request):
     posts = Post.objects.all()
@@ -13,10 +15,35 @@ def post_detail(request, id):
     return render(request, 'post/detail.html', context)
 
 def post_create(request):
-    return HttpResponse('create')
+    form = PostForm()
+    context = {
+        'form': form,
+    }
 
-def post_update(request):
-    return HttpResponse('update')
+    form = PostForm(request.POST or None)
+    if form.is_valid():
+        post = form.save()
+        messages.success(request, 'Post created succesfully!')
+        return HttpResponseRedirect(post.get_absolute_url())
 
-def post_delete(request):
-    return HttpResponse('delete')
+
+    return render(request, 'post/form.html', context)
+
+def post_update(request, id):
+    post = get_object_or_404(Post, id = id)
+    form = PostForm(request.POST or None, instance = post)
+    if form.is_valid():
+        form.save()
+        messages.success(request, 'Post updated succesfully!')
+        return HttpResponseRedirect(post.get_absolute_url())
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'post/form.html', context)
+
+def post_delete(request, id):
+    post = get_object_or_404(Post, id = id)
+    post.delete()
+    return redirect('post:index')
