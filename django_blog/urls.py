1f51cfdf0b848a1814eb7a058a1cbf0fcf4c9c3f
erdem <demirkapierdem@gmail.com>
2019-07-12 11:54:35 +0300

    login and register

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('', home_view, name='home'),
 
     path('post/', include('post.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
